f7be5b18a6f65dae4d1f2fb87bf060deb2fa08f4
Rongsong Shen <rongsong.shen@gmail.com>
2016-12-28 17:49:13 +0800

    save work status

--- examples/csv-convert/csv-convert.cpp
+++ examples/csv-convert/csv-convert.cpp
@@ -13,6 +13,9 @@
 #include <string>
 
 #include "boost/scoped_ptr.hpp"
+#include "boost/tokenizer.hpp"
+#include "boost/spirit/include/qi.hpp"
+
 #include "caffe/proto/caffe.pb.h"
 #include "caffe/util/db.hpp"
 #include "caffe/util/format.hpp"
@@ -20,6 +23,28 @@
 using namespace caffe;
 using boost::scoped_ptr;
 using std::string;
+
+namespace qi = boost::spirit:qi;
+
+int
+parse_line(const std::string s,
+           int *nfields,
+           int *data)
+{
+    typedef boost::tokenizer< boost::escaped_list_separator<char> , std::string::const_iterator, std::string> Tokenizer;
+    boost::escaped_list_separator<char> seps('\\', ',', '\"');
+    Tokenizer tok(s, seps);
+    int   i;
+
+    i = 0;
+    for (auto v: tok) {
+        data[i] = atoi(v);
+        i ++;
+    }
+
+    *nfields = i;
+    return 1;
+}
 
 int
 convert_dataset(const char *data_file,
