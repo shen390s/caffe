a66c0b9828333c689190c19e951ea277f5adfdd8
Rongsong Shen <rongsong.shen@gmail.com>
2016-12-28 17:25:12 +0800

    save work status

---
+++ examples/csv-convert/csv-convert.cpp
@@ -0,0 +1,28 @@
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <gooogle/protobuf/text_format.h>
+
+#include <leveldb/db.h>
+#include <leveldb/write_batch.h>
+#include <lmdb.h>
+
+#include <stdint.h>
+#include <sys/stat.h>
+
+#include <fstream>
+#include <string>
+
+#include "boost/scoped_ptr.hpp"
+#include "caffe/proto/caffe.pb.h"
+#include "caffe/util/db.hpp"
+#include "caffe/util/format.hppp"
+
+using namespace caffe;
+using boost::scoped_ptr;
+using std::string;
+
+int
+main(int argc, char *argv[])
+{
+    return 0;
+}
