c2885571b2a66c106d973d284b95123d6016a417
Rongsong Shen <rongsong.shen@gmail.com>
2016-12-30 11:07:09 +0800

    save work status

--- examples/csv-convert/csv-convert.cpp
+++ examples/csv-convert/csv-convert.cpp
@@ -87,7 +87,7 @@ convert_dataset(const char *data_file,
             break;
         }
 
-        datum.set_channels(1);
+        datum.set_channels(4);
         datum.set_height((nfields-1));
         datum.set_width(1 /*sizeof(data[0])*/);
         datum.set_data(data, sizeof(data[0])*(nfields-1));
